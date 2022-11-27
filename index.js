b81bfcc2126a50822b5e9b8767a10effa09fb553
asif3062 <asifbsb2002@gmail.com>
2022-11-27 20:41:03 +0600

    mongodb added

--- index.js
+++ index.js
@@ -2,13 +2,45 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors())
 
-const phones = require('./phones.json')
+/* 
+
+database : mobileBazar
+collections : products
+
+dbUser : mobileBazar
+pass : QphRrjAszKBZYINx
+*/
 
 app.get('/',(req,res)=>{
-    res.send(phones);
+    res.send('server running');
 })
+
+
+
+const uri = "mongodb+srv://mobileBazar:QphRrjAszKBZYINx@cluster0.mtnbd39.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const phonesCollection = client.db('mobileBazar').collection('products');
+
+        app.get('/phones',async(req,res)=>{
+            const query = {}
+            const phones = await phonesCollection.find(query).toArray()
+            res.send(phones)
+
+        })
+    }
+    finally{
+
+    }
+
+}
+run().catch(err=>console.log(err))
+
 
 app.listen(port,()=>{
     console.log(`http://localhost:${port}`)
