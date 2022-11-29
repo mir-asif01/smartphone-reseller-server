1779436a46090772d011ee7872ba3bd512e59e4c
asif3062 <asifbsb2002@gmail.com>
2022-11-29 19:54:45 +0600

    user deleting api added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dbUser : mobileBazar
 pass : QphRrjAszKBZYINx
 */
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('server running');
 })
 
@@ -24,34 +24,34 @@ app.get('/',(req,res)=>{
 const uri = "mongodb+srv://mobileBazar:QphRrjAszKBZYINx@cluster0.mtnbd39.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         const phonesCollection = client.db('mobileBazar').collection('products');
         const userCollection = client.db("mobileBazar").collection('users')
         const orderCollection = client.db("mobileBazar").collection('orders')
         const adCollection = client.db("mobileBazar").collection('advertisements')
 
-        app.get('/phones',async(req,res)=>{
+        app.get('/phones', async (req, res) => {
             const query = {}
             const phones = await phonesCollection.find(query).toArray()
             res.send(phones)
 
         })
-        app.get('/phones/:category',async(req,res)=>{
+        app.get('/phones/:category', async (req, res) => {
             const category = req.params.category
-            const query = { category : category}
+            const query = { category: category }
             const phone = await phonesCollection.find(query).toArray()
             res.send(phone)
         })
-        app.get('/categories',async(req,res)=>{
+        app.get('/categories', async (req, res) => {
             const query = {}
             const phones = await phonesCollection.find(query).toArray();
             const categories = []
-            phones.forEach(phone=>{
-                if(categories.includes(phone.category)){
+            phones.forEach(phone => {
+                if (categories.includes(phone.category)) {
                     return;
                 }
-                else{
+                else {
                     categories.push(phone.category)
                 }
             })
@@ -59,74 +59,88 @@ async function run(){
         })
 
         // users
-        app.post('/users',async(req,res)=>{
+        app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user)
             res.send(result)
         })
 
-        app.get('/users',async(req,res)=>{
-            const email = req.query.email;
-            const query = {email : email}
-            const user = await userCollection.findOne(query)
-            res.send(user)
+        app.get('/users', async (req, res) => {
+            const query = {}
+            if (req.query.email) {
+                const email = req.query.email;
+                const query = { email: email }
+                const user = await userCollection.findOne(query)
+                res.send(user)
+            }
+            else {
+                const result = await userCollection.find(query).toArray()
+                res.send(result)
+            }
+        })
+
+        app.delete('/users/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)}
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
         })
 
         //orders
-        app.post('/orders',async(req,res)=>{
+        app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order)
             res.send(result)
         })
-        app.get('/orders',async(req,res)=>{
+        app.get('/orders', async (req, res) => {
             const email = req.query.email;
-            const query = {email : email}
+            const query = { email: email }
             const orders = await orderCollection.find(query).toArray()
             res.send(orders)
         })
 
         //my products
-        app.get('/products',async(req,res)=>{
+        app.get('/products', async (req, res) => {
             const email = req.query.email;
-            const query = {seller_email : email}
+            const query = { seller_email: email }
             const products = await phonesCollection.find(query).toArray()
             res.send(products)
         })
 
-        app.post('/products',async(req,res)=>{
+        app.post('/products', async (req, res) => {
             const product = req.body;
             const result = await phonesCollection.insertOne(product)
             res.send(result)
         })
 
-        app.delete('/products/:id',async(req,res)=>{
+        app.delete('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const result = phonesCollection.deleteOne(query)
             res.send(result)
         })
 
         // advertisement
 
-        app.post('/advertisements',async(req,res)=>{
+        app.post('/advertisements', async (req, res) => {
             const advertisement = req.body;
             const result = await adCollection.insertOne(advertisement)
             res.send(result)
         })
-        app.get('/advertisements',async(req,res)=>{
+        app.get('/advertisements', async (req, res) => {
             const query = {}
-            const ads = await adCollection.find(query).sort({time : -1}).toArray();
+            const ads = await adCollection.find(query).sort({ time: -1 }).toArray();
             res.send(ads);
         })
     }
-    finally{
+    finally {
 
     }
 
 }
-run().catch(err=>console.log(err))
+run().catch(err => console.log(err))
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`http://localhost:${port}`)
 })
