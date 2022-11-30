2722e5de2716889385512e1b8b71c11621210eca
asif3062 <asifbsb2002@gmail.com>
2022-11-30 19:29:33 +0600

    environment variable added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
@@ -10,9 +11,6 @@ app.use(express.json())
 
 database : mobileBazar
 collections : products,users,orders,advertisements
-
-dbUser : mobileBazar
-pass : QphRrjAszKBZYINx
 */
 
 app.get('/', (req, res) => {
@@ -21,7 +19,7 @@ app.get('/', (req, res) => {
 
 
 
-const uri = "mongodb+srv://mobileBazar:QphRrjAszKBZYINx@cluster0.mtnbd39.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mtnbd39.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
